9d644b32c92c8f0a9fbc4eb541527d22e76edf52
Anna Che <anna.cheredaryk@googlemail.com>
2021-11-16 17:48:40 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+/* //objects
+const obj = {
+    name: "Bella",
+    age: 25,
+    isMarried: false
+}
+/* console.log(obj.name) 
+console.log(obj["name"]);
+let arr = ['la.png', 'dhgf.png', 'ahsd.pmb', 6, {}, []];
+console.log(arr[0]); */
+
+
+//Urok 7
+/* //const result = confirm("ARe you here");
+//console.log(result);
+
+//const answer = +prompt('Re u 18 yo', '18');  // + преобразовали в инт
+//console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Your name', '');
+answers[1] = prompt('Your last name', '');
+answers[2] = prompt('How old are u', '');
+
+//document.write(answers);
+console.log(typeof(answers)); */
+
+
+// 8 Интерполяция  
+//const category = 'toys';
+//console.log('https:someurl.com/' + category); not right
+//console.log(`https:someurl.com/${category}/5`); 
+//const user = 'Anna';
+//alert(` Hi ${user}`);
+
+
+// 9 
+/* let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--; 
+
+console.log(incr++);
+console.log(decr--); */
